c15c04a8b54cf7238af7c6c31977a879ca1bad86
Várhegyi Balázs Zsolt <ezcryse@hotmail.com>
2022-05-08 17:42:22 +0200

    Dependency injection added

---
+++ app/src/main/java/com/example/nutracker/di/NetworkConfig.kt
@@ -0,0 +1,7 @@
+package com.example.nutracker.di
+
+object NetworkConfig {
+    const val ENDPOINT_ADDRESS = "https://www.fruityvice.com/"
+    const val ENDPOINT_PREFIX = "/api/"
+    const val SERVICE_ENDPOINT = ENDPOINT_ADDRESS + ENDPOINT_PREFIX
+}
--- app/src/main/java/com/example/nutracker/di/PersistenceModule.kt
+++ app/src/main/java/com/example/nutracker/di/PersistenceModule.kt
@@ -1,10 +1,34 @@
 package com.example.nutracker.di
 
+import android.app.Application
+import androidx.room.Room
+import com.example.nutracker.R
+import com.example.nutracker.persistence.AppDatabase
+import com.example.nutracker.persistence.NutrackerDao
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object PersistenceModule {
+
+    @Provides
+    @Singleton
+    fun provideAppDatabase(application: Application): AppDatabase {
+        return Room.databaseBuilder(
+                application,
+                AppDatabase::class.java,
+                "app_database"
+            )
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNutrackerDao(appDatabase: AppDatabase): NutrackerDao {
+        return appDatabase.nutrackerDao()
+    }
 }
