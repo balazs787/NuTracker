4da6aeb188db08dbcee6db374bf7acdd7311c614
Várhegyi Balázs Zsolt <ezcryse@hotmail.com>
2022-05-08 17:43:10 +0200

    Network and persistence updated

--- app/src/main/java/com/example/nutracker/persistence/AppDatabase.kt
+++ app/src/main/java/com/example/nutracker/persistence/AppDatabase.kt
@@ -1,4 +1,10 @@
 package com.example.nutracker.persistence
 
-class AppDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.nutracker.model.Fruit
+
+@Database(entities = [Fruit::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun nutrackerDao(): NutrackerDao
 }
--- app/src/main/java/com/example/nutracker/persistence/NutrackerDao.kt
+++ app/src/main/java/com/example/nutracker/persistence/NutrackerDao.kt
@@ -1,4 +1,23 @@
 package com.example.nutracker.persistence
 
-class NutrackerDao {
+import androidx.room.*
+import com.example.nutracker.model.Fruit
+
+@Dao
+interface NutrackerDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertFruit(fruit: Fruit)
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    fun updateFruit(fruit: Fruit)
+
+    @Delete
+    fun deleteFruit(fruit: Fruit)
+
+    @Query("SELECT * FROM Fruit")
+    fun getFruits(): List<Fruit>
+
+    @Query("SELECT * FROM Fruit WHERE name = :name")
+    fun findById(name: String): Fruit?
 }
