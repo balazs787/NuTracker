e6bf21b5353fbc9a09c28c120a0eb4ccb2d8a69f
Várhegyi Balázs Zsolt <ezcryse@hotmail.com>
2022-05-22 16:21:37 +0200

    ViewModel update

--- app/src/main/java/com/example/nutracker/ui/main/MainRepository.kt
+++ app/src/main/java/com/example/nutracker/ui/main/MainRepository.kt
@@ -1,10 +1,33 @@
 package com.example.nutracker.ui.main
 
+import androidx.annotation.WorkerThread
+import com.example.nutracker.model.Fruit
 import com.example.nutracker.network.NutrackerService
 import com.example.nutracker.persistence.NutrackerDao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class MainRepository @Inject constructor(
-    nuTrackerService: NutrackerService,
-    nutrackerDao: NutrackerDao
-)
+    private val nutrackerService: NutrackerService,
+    private val nutrackerDao: NutrackerDao
+){
+    @WorkerThread
+    fun loadFruits(onStart: () -> Unit, onCompletion: () -> Unit) = flow {
+        val fruits: List<Fruit>? = nutrackerService.getFruits().execute().body()
+        emit(fruits)
+    }.onStart { onStart() }.onCompletion { onCompletion() }.flowOn(Dispatchers.IO)
+
+    fun add(fruit: Fruit) {
+        GlobalScope.launch{ nutrackerDao.insertFruit(fruit) }
+    }
+
+    fun delete(fruit: Fruit) {
+        GlobalScope.launch{ nutrackerDao.deleteFruit(fruit) }
+    }
+}
--- app/src/main/java/com/example/nutracker/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/nutracker/ui/main/MainViewModel.kt
@@ -1,4 +1,80 @@
 package com.example.nutracker.ui.main
 
-class MainViewModel {
+import androidx.annotation.StringRes
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.nutracker.model.Fruit
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    mainRepository: MainRepository
+): ViewModel(){
+    @Inject lateinit var mainRepository: MainRepository;
+
+    private val _isLoading: MutableState<Boolean> = mutableStateOf(false)
+    val isLoading: State<Boolean> get() = _isLoading
+
+    private val _details: MutableState<Boolean> = mutableStateOf(false)
+    val details: State<Boolean> get() = _details
+
+    private val _selectedTab: MutableState<Int> = mutableStateOf(0)
+    val selectedTab: State<Int> get() = _selectedTab
+
+    private val _selectedFruit: MutableState<Fruit> = mutableStateOf(Fruit())
+    val selectedFruit: State<Fruit> get() = _selectedFruit
+
+    private val _title: MutableState<String> = mutableStateOf("Fruits")
+    val title: State<String> get() = _title
+
+
+    val fruitList: Flow<List<Fruit>?> =
+        mainRepository.loadFruits(
+            onStart = { _isLoading.value = true },
+            onCompletion = { _isLoading.value = false }
+        )
+
+    fun selectFruit(tab: Int, fruit: Fruit) {
+        _selectedTab.value = tab;
+        _selectedFruit.value = fruit;
+        _title?.value = fruit?.name?:"";
+        _details.value = true;
+        println(selectedFruit.toString());
+    }
+
+    fun delete(fruit: Fruit){
+        mainRepository.delete(fruit);
+    }
+
+    fun backClicked(){
+        _selectedTab.value = 0;
+        _title.value = "Fruit";
+        _details.value = false;
+    }
+
+    fun addClicked(){
+        _selectedTab.value = 1;
+        _title.value = "Add fruit";
+        _selectedFruit.value = Fruit();
+        _details.value = true;
+    }
+
+    fun saveClicked(){
+        mainRepository.add(_selectedFruit.value);
+        _title.value = "Save";
+        _details.value = false;
+        _selectedTab.value = 0;
+    }
+
+
+    fun selectTab(@StringRes tab: Int) {
+        _selectedTab.value = tab
+    }
+
+    fun <T : Any?> MutableLiveData<T>.default(initialValue: T) = apply { setValue(initialValue) }
 }
